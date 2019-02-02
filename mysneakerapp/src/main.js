806944ca2bb2bae1f8517b3da50887ea9f669fc5
gomi <gxy880520@qq.com>
2019-02-02 10:20:08 +0800

    new

--- mysneakerapp/src/main.js
+++ mysneakerapp/src/main.js
@@ -4,17 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from '@iview';
-import '@iview/dist/styles/iview.css'
-import store from './store/base-store';
+import 'iview/dist/styles/iview.css'
+//import store from './store/base-store';
 
 Vue.config.productionTip = false;
-Vue.use(iView)
+Vue.use(iView);
+//Vue.use(store);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store,
+  //store,
   components: { App },
   template: '<App/>'
 })
