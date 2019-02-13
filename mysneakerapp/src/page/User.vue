1a727257b4621f54b8537f56c0d360e2ef99ec3a
2455065858@qq.com <2455065858@qq.com>
2019-02-13 11:18:01 +0800

    wy修改

---
+++ mysneakerapp/src/page/User.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <topUser></topUser>
+        <Activity></Activity>
+        <Gongneng></Gongneng>
+    </div>
+</template>
+<script>
+import topUser from './user/topUser';
+import Activity from './user/Activity';
+import Gongneng from './user/Gongneng';
+export default {
+    name:'User',
+    components:{
+        topUser,
+        Activity,
+        Gongneng
+    }
+}
+</script>
+
+<style scoped lang="scss">
+#app{
+    margin: 0 ;
+}
+</style>
